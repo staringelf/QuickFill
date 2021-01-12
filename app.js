294790171b0a7fbddb147e0650af67422350e4b9
Manish Sajwan <msoffice37@gmail.com>
2021-01-12 01:49:19 -0800

    add dynamic ability

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const model = {
   pinCode: null,
 
   userData: {
-    name: 'ABc',
+    name: null,
     email: null,
     phone: null,
     address: null,
@@ -35,9 +35,10 @@ const view = {
   },
 
   setUpEventListeners () {
-    this.pinCodeInput.onblur = updatePinCode;
-    
-    this.formInputs.forEach(formInput => formInput.onblur = updateUserData);
+
+    this.pinCodeInput.oninput = pinCodeInputHandler;
+
+    this.formInputs.forEach(formInput => formInput.onblur = takeUserData);
   },
 
   render (data) {
@@ -49,30 +50,97 @@ const view = {
 
 }
 
+const postalUrl = `https://api.postalpincode.in/pincode/`;
+
 const getModelData = function () {
   return model;
 }
 
-const updatePinCode = function () {
-  if(this.value == null){
-    return;
-  }
-
-  model.pinCode = this.value;
-  createRequest(createUrl());
+const pinCodeInputHandler = function () {
+  resetUserData.call(this);
+  updatePinCode.call(this);
 }
 
-const updateUserData = function () {
+const updatePinCode = function () {
+  if(this.value == null || this.value.length !== 6)    
+    return;
+
+  model.pinCode = this.value;
+  model.userData['zip-code'] = model.pinCode;
+  createRequest(createPostalUrl());
+}
+
+const resetUserData = function () {
+  if(this.value == null || this.value.length !== 6)    
+    return;
+  const userData = model.userData;
+  for (const prop in userData){
+  userData[prop] = null;
+  }
+  view.render(model);
+}
+
+const takeUserData = function () {
   if(this.value != null){
     model.userData[this.id] = this.value;
   }
 }
 
-const postalUrl = `https://api.postalpincode.in/pincode/`;
-
 const postalUpdatePageSuccess = function (data) {
+
   const parsedData = JSON.parse(data);
   console.log(parsedData);
+  const places = parsedData[0].PostOffice;
+
+  if(places === null) {
+    const choiceChipsDiv = document.querySelector('.choice_chips');
+    choiceChipsDiv.innerHTML = `<p>
+      Sorry! Invalid Pin Code
+    </p>`;
+    choiceChipsDiv.classList.add('invalid_pin');
+    choiceChipsDiv.parentNode.classList.add('unpad');
+    return;
+  }
+  
+  const placeInfo = {
+    country: places[0].Country,
+    city: places[0].Block,
+    district: places[0].District,
+    state: places[0].State,
+  }
+  const addresses = places.map(place => place.Name);
+
+  if(addresses.length){
+    const choiceChipsDiv = document.querySelector('.choice_chips');
+    choiceChipsDiv.innerHTML = '';
+    choiceChipsDiv.classList.remove('invalid_pin');
+    choiceChipsDiv.parentNode.classList.remove('unpad');
+    addresses.forEach(address => {
+      const choiceDiv = document.createElement('div');
+      choiceDiv.classList.add('choice_chip');
+      choiceDiv.innerHTML = `<p>${address}</p>`;
+      choiceDiv.onclick = choiceClickHandler;
+      choiceChipsDiv.appendChild(choiceDiv);
+    })
+  }
+
+  updateUserData(placeInfo);
+  view.render(model);
+}
+
+const updateUserData = function(placeInfo) {
+  
+  const userData = model.userData;
+
+  for (const prop in placeInfo){
+    userData[prop] = placeInfo[prop];
+  } 
+}
+
+const choiceClickHandler = function () {
+  document.querySelectorAll('.choice_chip').forEach(choiceChip => choiceChip !== this ? choiceChip.classList.remove('selected') : choiceChip.classList.add('selected') );
+  model.userData.address = this.textContent;
+  view.render(model);
 }
 
 const postalUpdatePageError = function (error) {
@@ -83,9 +151,9 @@ const responseMethod = function (httpRequest, succeed, failure) {
   if(httpRequest.readyState === 4){
     if(httpRequest.status === 200){
       succeed(httpRequest.responseText);
+    } else {
+      failure(httpRequest.status + ':' + httpRequest.responseText);
     }
-  } else {
-    failure(httpRequest.status + ':' + httpRequest.responseText);
   }
 }
 
@@ -96,8 +164,8 @@ const createRequest = function (url) {
   httpRequest.send();
 }
 
-const createUrl = function () {
-    return postalUrl + model.pinCode; 
+const createPostalUrl = function () {
+    return postalUrl + model.pinCode;
 }
 
 view.init();
